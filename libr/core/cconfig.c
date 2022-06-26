f138a830055735f042f7b7387791d3051865bacd
pancake <pancake@nopcode.org>
2022-06-26 03:25:16 +0200

    Enable log.origin

--- libr/core/cconfig.c
+++ libr/core/cconfig.c
@@ -3752,9 +3752,9 @@ R_API int r_core_config_init(RCore *core) {
 	SETCB ("log.ts", "false", cb_log_config_ts, "Show timestamp in log messages");
 
 	SETICB ("log.traplevel", 0, cb_log_config_traplevel, "Log level for trapping R2 when hit");
-	SETCB ("log.file", "", cb_log_config_file, "Logging output filename / path");
+	SETCB ("log.file", "", cb_log_config_file, "Save log messages to given filename"); // 580 -rename to file.log ?)
 	SETCB ("log.filter", "", cb_log_config_filter, "Filter only messages matching given origin");
-	SETCB ("log.origin", "false", cb_log_origin, "Show [origin] in log messages");
+	SETCB ("log.origin", "true", cb_log_origin, "Show [origin] in log messages");
 	SETCB ("log.color", "false", cb_log_config_colors, "Should the log output use colors");
 	SETCB ("log.quiet", "false", cb_log_config_quiet, "Be quiet, dont log anything to console");
 
